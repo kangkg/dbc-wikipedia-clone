4192c6a0adf6ad293a49bde9a7270bd4bed8f5eb
Kenneth Kang <email": null>
2016-12-08 14:14:50 -0600

    fixed up some specs

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
-feature 'user registration' do
-  scenario 'user can create an account' do
-      visit new_user_path
+describe 'User Features' do
+  feature 'user registration' do
+    scenario 'user can create an account' do
+        visit new_user_path
 
-      find('#user_username').set('ilovepie')
-      find('#user_password').set('password')
-      click_on "Submit"
-      expect(page).to have_content("Logout!")
+        find('#user_username').set('ilovepie')
+        find('#user_password').set('password')
+        click_on "Submit"
+        expect(page).to have_content("Logout")
+    end
   end
 end
--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -1,27 +1,28 @@
 require 'rails_helper'
 
-feature 'user can login and logout' do
-  scenario 'user can login' do
-    User.create(username: 'piepie123', password: 'piepie123', role: 'user')
-    visit root_path
+describe 'Sessions Features' do
+  feature 'user can login and logout' do
+    scenario 'user can login' do
+      User.create(username: 'piepie123', password: 'piepie123', role: 'user')
+      visit root_path
 
-    click_on "Login!"
-    find('#sessions_username').set('piepie123')
-    find('#sessions_password').set('piepie123')
-    click_on "Submit"
-    expect(page).to have_content("Logout!")
-  end
+      click_on "Login"
+      find('#sessions_username').set('piepie123')
+      find('#sessions_password').set('piepie123')
+      click_on "Submit"
+      expect(page).to have_content("Logout!")
+    end
 
-  scenario 'user can logout' do
-    User.create(username: 'piepie123', password: 'piepie123', role: 'user')
-    visit root_path
+    scenario 'user can logout' do
+      User.create(username: 'piepie123', password: 'piepie123', role: 'user')
+      visit root_path
 
-    click_on "Login!"
-    find('#sessions_username').set('piepie123')
-    find('#sessions_password').set('piepie123')
-    click_on "Submit"
-    click_on "Logout!"
-    expect(page).to have_content("Login!")
+      click_on "Login"
+      find('#sessions_username').set('piepie123')
+      find('#sessions_password').set('piepie123')
+      click_on "Submit"
+      click_on "Logout!"
+      expect(page).to have_content("Login")
+    end
   end
 end
-

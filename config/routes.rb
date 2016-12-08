ac3082243c34df8b4d9b21dcae0aedb8106f9386
Kenneth Kang <email": null>
2016-12-08 10:41:47 -0600

    add category route and rspec notes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
   resources :articles, only: [:index, :new, :create, :destroy, :show]
+  resources :categories # not sure if we should allow routes for this.
 
   delete '/logout' => 'sessions#destroy'
   delete '/favorites' => 'favorites#destroy'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207204032) do
+ActiveRecord::Schema.define(version: 20161208161858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20161207204032) do
     t.integer  "author_id",  null: false
     t.string   "status",     null: false
     t.string   "title",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

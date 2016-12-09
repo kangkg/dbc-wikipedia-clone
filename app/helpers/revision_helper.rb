71c14487479166b9ebcf5f642020a985fb232af9
Matthew Ramuta <matthew.g.ramuta@gmail.com>
2016-12-09 15:44:22 -0600

    remove extraneous print lines

--- app/helpers/revision_helper.rb
+++ app/helpers/revision_helper.rb
@@ -3,14 +3,12 @@ module RevisionHelper
   def body_with_links_and_images(revision)
     body_str = revision.body.dup
     body_str = add_links(body_str)
-    p body_str.html_safe
     body_str = add_images(body_str)
     sanitize(body_str.html_safe,tags: %w(strong em img a p u br hr),attributes:%w(src style href))
   end
 
   def add_links(str)
     links = Link.all
-    p links
     links.each do |link|
       str.gsub!(/(#{link.word})/, link_to(link.word, article_path(Article.find(link.article_id)))+" ")
     end

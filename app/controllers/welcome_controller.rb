7b993ed7f360e423d480fbf21e8efa411aa945c4
Vince Roti <vince.roti@gmail.com>
2016-12-08 23:18:19 -0600

    bug fix, front page now shows body of featured article

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
 
   def index
     @featured_article = Article.find_by(featured: true)
+    @last_revision = @featured_article.revisions.last
   end
 
 end

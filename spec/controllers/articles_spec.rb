79b405fdb629291866bc30045320f9896a058d53
Kenneth Kang <kenneth.g.kang@vanderbilt.edu>
2016-12-09 15:50:41 -0600

    finished up some tests after changes

--- spec/controllers/articles_spec.rb
+++ spec/controllers/articles_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 describe ArticlesController do
   let!(:user) { User.create(username: 'a', password: 'b', role: 'user') }
   let!(:article) { Article.create!(title: "Article", author_id: user.id, status: "published") }
-  let(:unpublished_article) { Article.create!(title: "unpub-arty", author_id: user.id, status: "unpublished") }
+  let!(:category) { Category.create!(name: "category man") }
+  let!(:unpublished_article) { Article.create!(title: "unpub-arty", author_id: user.id, status: "unpublished") }
 
   describe "GET #show" do
     it "responds with status code 200" do
@@ -16,10 +17,6 @@ describe ArticlesController do
       expect(response).to render_template(:show)
     end
 
-    it "renders the 403 page for unpublished pages" do
-      get :show, params: { id: unpublished_article.id }
-      expect(response).to have_http_status 403
-    end
   end
 
   describe "GET #new" do
@@ -38,46 +35,54 @@ describe ArticlesController do
     before (:each) do
       controller.session[:user_id] = user.id
     end
+
     context "when valid params are passed" do
       it "responds with status code 302" do
-        post :create, params: { article: { title: "Article", author_id: user.id, status: "unpublished" } }
+        post :create, params: {
+          article: { title: "Article", author_id: user.id, status: "unpublished", category: {name: category.name } }
+        }
         expect(response).to have_http_status 302
       end
 
       it "assigns a new article in the DB" do
-        expect{post :create, params: { article: { title: "Article", author_id: user.id, status: "unpublished" } } }
+        expect{ post :create, params: {
+          article: { title: "Article", author_id: user.id, status: "unpublished", category: {name: category.name } } } }
         .to change(Article, :count).by(1)
       end
 
       it "assigns the newly created article as @article" do
-        post :create, params: { article: { title: "Article", author_id: user.id, status: "unpublished" } }
+        post :create, params: {
+          article: { title: "Article", author_id: user.id, status: "unpublished", category: {name: category.name } }
+        }
         expect(assigns(:article)).to eq (Article.last)
       end
 
       it "redirects to the created game" do
-        post :create, params: { article: { title: "Article", author_id: user.id, status: "unpublished" } }
+        post :create, params: {
+          article: { title: "Article", author_id: user.id, status: "unpublished", category: {name: category.name } }
+        }
         expect(response).to redirect_to (Article.last)
       end
     end
 
     context "when invalid params are passed" do
       it "responds with status code 200" do
-        post :create, params: { article: { author_id: user.id, status: "unpublished" } }
+        post :create, params: { article: { status: "unpublished", category: {name: category.name }}}
         expect(response).to have_http_status 200
       end
 
       it "does not create a new article in the database" do
-        expect{post :create, params: { article: { author_id: user.id, status: "unpublished" } } }
+        expect{ post :create, params: { article: { status: "unpublished", category: {name: category.name }}} }
         .to change(Article, :count).by(0)
       end
 
       it "assigns the unsaved article as @article" do
-        post :create, params: { article: { author_id: user.id, status: "unpublished" } }
+        post :create, params: { article: { status: "unpublished", category: {name: category.name }}}
         expect(assigns(:article)).not_to eq(Article.last)
       end
 
       it "renders the :new template" do
-        post :create, params: { article: { author_id: user.id, status: "unpublished" } }
+        post :create, params: { article: { author_id: user.id, status: "unpublished", category: {name: category.name }}}
         expect(response).to render_template(:new)
       end
     end

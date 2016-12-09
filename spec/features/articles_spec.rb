d2eee1cf00785a99e2db9bab014484b50a013182
Eraince <eraince@hotmail.com>
2016-12-09 11:35:09 -0600

    user profile finished.

--- spec/features/articles_spec.rb
+++ spec/features/articles_spec.rb
@@ -15,6 +15,20 @@ feature 'articles' do
     expect(page).to have_current_path article_path(article)
   end
 
+  # scenario 'admin or superadmin can featured the article' do
+  #   user = User.create!(username: 'piepie123', password: 'piepie123', role: 'admin')
+  #   article = Article.create!(author_id: user.id, title: "Jay Chou", status: "published", featured: false)
+  #   revisions = Revision.create!(editor_id: user.id, article_id: article.id, body: "Jay Chou (born 18 January 1979) is a Taiwanese musician, singer, songwriter, record producer, actor, and director.")
+  #   visit articles_path
+
+  #   within('#articles') do
+  #     expect(page).to have_content article.title
+  #     click_link("Jay Chou")
+  #   end
+
+    expect(page).to have_current_path article_path(article)
+  end
+
   xscenario 'user can create a new article if they are logged in' do
 
   end

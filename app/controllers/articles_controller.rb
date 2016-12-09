a7bcf99734e890fc3d53ef8e8cc609e3153225bd
Kenneth Kang <kenneth.g.kang@vanderbilt.edu>
2016-12-09 14:55:17 -0600

    made many different css changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,13 +6,21 @@ class ArticlesController < ApplicationController
 
   def new
     @article = Article.new
+    # @sorted_article = SortedArticle.new
+    @revision = Revision.new
   end
 
   def create
     if logged_in?
       @article = Article.new(article_params)
-      @article.author_id = current_user.id
-      if @article.save
+      @article.author = current_user
+
+      desired_category = Category.find_or_create_by(category_params)
+
+      # @article.sorted_articles << new_sorted_article
+
+      if @article.save && desired_category.save # need revision to be created as well. order also matters here
+        SortedArticle.create(category_id: desired_category.id, article_id: @article.id)
         redirect_to @article
       else
         @errors = @article.errors.full_messages
@@ -25,17 +33,21 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @author = @article.author.username
     @last_revision = @article.revisions.last
-    if @article.status == "unpublished"
-      render :file => 'public/403.html', :status => 403, :layout => false
+    if @article.status == "unpublished" && current_user.try('role') == 'user' && current_user != @article.author
+      @article = nil
+      @errors = ['This page is under construction!']
+      render :show
     end
   end
+
+  # && ( current_user.try('role') == 'user' || current_user != @article.author)
 
   def update
     @article = Article.find(params[:id])
     @article.featured = !@article.featured
     @article.save
     respond_to do |format|
-        format.html { redirect_to @article }
+      format.html { redirect_to @article }
     end
   end
 
@@ -52,7 +64,14 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title)
+    params_hash = params.require(:article).permit(:title)
+    params_hash[:status] = 'unpublished'
+    params_hash
+  end
+
+  def category_params
+    category_hash = params.require(:article).require(:category).permit(:name)
+    # => {name: 'your category title'}
   end
 
 
